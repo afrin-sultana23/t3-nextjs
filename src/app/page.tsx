fd673c3e692dcd74424e69184af9d4a5e08d4d7c
afrin-sultana23 <afriin474@gmail.com>
2025-10-09 23:32:10 +0600

    Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
+import {NavigationMenu} from "~/components/ui/navigation-menu";
 
 export default function HomePage() {
   return (
     <main className="">
       <div className="container">
-        <h1 className="text-center text-5xl font-medium text-cyan-600">Hello There</h1>
+          <NavigationMenu />
+        <h1 className="text-center text-5xl font-medium text-cyan-600">Hello There
+
+        </h1>
       </div>
     </main>
   );
