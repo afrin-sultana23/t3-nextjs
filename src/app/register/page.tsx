99fa97d09bec4bb0a1fe7e613450443784fbe67c
afrin-sultana23 <afriin474@gmail.com>
2025-10-14 22:35:25 +0600

    register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import {useForm} from "react-hook-form";
 import {FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "~/components/ui/form";
 import {Input} from "~/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
 
 const Page = () => {
-            const form = useForm()
+            const form = useForm(
+            )
+
+            const formSchema = z.object({
+                username: z.string().min(2, {
+                    message: "Username must be at least 2 characters.",
+                }),
+            })
     return (
         <div>
             <h1>this is a register page</h1>
