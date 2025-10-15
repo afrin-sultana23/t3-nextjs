c1766720744a88da53b76719c5e4edb27584282f
afrin-sultana23 <afriin474@gmail.com>
2025-10-15 21:28:31 +0600

    new form

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import {useForm, SubmitHandler } from "react-hook-form";
-import {FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "~/components/ui/form";
-import {Input} from "~/components/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod"
-import { z } from "zod"
+"use client"
+import { type SubmitHandler, useForm} from "react-hook-form";
+// import {FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "~/components/ui/form";
+// import {Input} from "~/components/ui/input";
+// import { zodResolver } from "@hookform/resolvers/zod"
+// import { z } from "zod"
+
+
+type Inputs = {
+    username: string
+    password: any
+}
 
 const Page = () => {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            username: "",
-        },
-    })
-    const onSubmit: SubmitHandler<FormSchema> = (data) => console.log(data)
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<Inputs>()
+    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
 
-            const formSchema = z.object({
-                username: z.string().min(2, {
-                    message: "Username must be at least 2 characters.",
-                }),
-            })
     // function onSubmit(values: z.infer<typeof formSchema>) {
     //     // Do something with the form values.
     //     // ✅ This will be type-safe and validated.
@@ -27,21 +28,32 @@ const Page = () => {
     return (
         <div>
             <h1>this is a register page</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                {/* register your input into the hook by invoking the "register" function */}
+                <input type="text" placeholder="username" {...register("username")} />
 
-            <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Username</FormLabel>
-                        <FormControl>
-                            <Input placeholder="place your input" {...field} />
-                        </FormControl>
-                        <FormDescription>This is your public display name.</FormDescription>
-                        <FormMessage />
-                    </FormItem>
-                )}
-            />
+                {/* include validation with required or other standard HTML validation rules */}
+                <input type="password" placeholder="password" {...register("password", { required: true })} />
+                {/* errors will return when field validation fails  */}
+                {errors.password && <span>This field is required</span>}
+
+                <input type="submit" />
+            </form>
+
+            {/*<FormField*/}
+            {/*    control={form.control}*/}
+            {/*    name="username"*/}
+            {/*    render={({ field }) => (*/}
+            {/*        <FormItem>*/}
+            {/*            <FormLabel>Username</FormLabel>*/}
+            {/*            <FormControl>*/}
+            {/*                <Input placeholder="place your input" {...field} />*/}
+            {/*            </FormControl>*/}
+            {/*            <FormDescription>This is your public display name.</FormDescription>*/}
+            {/*            <FormMessage />*/}
+            {/*        </FormItem>*/}
+            {/*    )}*/}
+            {/*/>*/}
         </div>
     );
 };
