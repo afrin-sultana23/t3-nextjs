e5e388aae23fe17683fda6ed598c6ba0065acbf3
afrin-sultana23 <afriin474@gmail.com>
2025-10-16 22:31:42 +0600

    new login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {useForm} from "react-hook-form";
+
+const Page = () => {
+
+    const form = useForm<any>({
+
+    })
+    const onsubmit = (data: any) => {}
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Page;
