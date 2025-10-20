810298c6c3a00195f3dcdb4c292d1cdf969a377e
afrin-sultana23 <afriin474@gmail.com>
2025-10-20 13:34:38 +0600

    footer section

--- src/components/Home/footer.tsx
+++ src/components/Home/footer.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <div>e_shop</div>
-            <div>Contacts</div>
+        <div className="grid grid-cols-1 md:grid-cols-4">
+            <div>
+                <p>e_shop</p>
+                <p>80% discount </p>
+            </div>
+            <div>Contacts
+                <p>Admin</p>
+            </div>
             <div>Marketing</div>
             <div>Category</div>
         </div>
