4f98925d0235ee041730660a9b74881ccd23224b
afrin-sultana23 <afriin474@gmail.com>
2025-10-24 22:55:59 +0600

    update

--- src/components/products/item.tsx
+++ src/components/products/item.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Card, CardDescription, CardHeader} from "~/components/ui/card";
 
 const Item = () => {
     return (
         <div>
-
+            <Card>
+                <CardHeader>
+                    <CardDescription>Shoes</CardDescription>
+                </CardHeader>
+            </Card>
         </div>
     );
 };
