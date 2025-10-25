de6566eeb2cb27365c208c5c3998375362a1603d
afrin-sultana23 <afriin474@gmail.com>
2025-10-25 23:12:12 +0600

    update

--- src/components/products/item.tsx
+++ src/components/products/item.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import {Card, CardDescription, CardHeader} from "~/components/ui/card";
+import {Card, CardAction, CardDescription, CardHeader, CardTitle} from "~/components/ui/card";
 
 const Item = () => {
     return (
         <div>
             <Card>
                 <CardHeader>
+                    <CardTitle>
+                        <img/>
+                    </CardTitle>
                     <CardDescription>Shoes</CardDescription>
+                    <CardAction></CardAction>
+
                 </CardHeader>
             </Card>
         </div>
