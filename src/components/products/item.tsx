31338538281eef5cc0380eec1997f06de6881a8c
afrin-sultana23 <afriin474@gmail.com>
2025-10-25 23:14:58 +0600

    update

--- src/components/products/item.tsx
+++ src/components/products/item.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import {Card, CardAction, CardDescription, CardHeader, CardTitle} from "~/components/ui/card";
+import {Alert} from "~/components/ui/alert";
+import {BadgeAlert} from "lucide-react";
 
 const Item = () => {
     return (
         <div>
             <Card>
                 <CardHeader>
+                    <Alert>
+                        <BadgeAlert/>
+                    </Alert>
                     <CardTitle>
-                        <img/>
+                        <img alt=""/>
                     </CardTitle>
                     <CardDescription>Shoes</CardDescription>
                     <CardAction></CardAction>
