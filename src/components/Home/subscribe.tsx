457d038a488d56ab72c5280f9d9f9b82800073ac
afrin-sultana23 <afriin474@gmail.com>
2025-11-03 22:15:54 +0600

    update email submit

--- src/components/Home/subscribe.tsx
+++ src/components/Home/subscribe.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Subscribe = () => {
+
+    const [email, setEmail] = useState('');
+    const [status, setStatus] = useState('');
+    const handleSubmit = () => {
+        e.preventDefault();
+    }
+
     return (
         <div>
 
