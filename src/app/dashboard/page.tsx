5907d7e743ae03a6520d41a88ec82df0f7e068db
afrin-sultana23 <afriin474@gmail.com>
2025-11-10 00:53:23 +0600

    dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import productCard from "~/components/products/productCard";
 
 const Page = () => {
+    const rows =[
+        productCard
+    ]
     return (
         <div>
-            
+            <picture>"https://ibb.co.com/SXf82mjT"</picture>
         </div>
     );
 };
