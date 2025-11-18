606682240e20f4565d027e93635caad47c779b23
afrin-sultana23 <afriin474@gmail.com>
2025-11-18 21:49:44 +0600

    update

--- src/components/Home/topbar.tsx
+++ src/components/Home/topbar.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 const Topbar = () => {
     return (
         <div>
-            <p>10%  discount</p>
+            <div className="w-full flex justify-between mx-auto p-3">
+                <div></div>
+                <div></div>
+            </div>
+           <p>10%  discount</p>
 
 
         </div>
