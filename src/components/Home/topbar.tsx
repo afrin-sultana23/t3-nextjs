88ae8637bf089a2e5704754e98eca2e8f0d43829
afrin-sultana23 <afriin474@gmail.com>
2025-11-19 20:10:22 +0600

    update

--- src/components/Home/topbar.tsx
+++ src/components/Home/topbar.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import Link from "next/link";
 
 const Topbar = () => {
     return (
         <div>
-            <div className="w-full flex justify-between mx-auto p-3">
+            <div className="max-w-5xl flex justify-between mx-auto p-3">
                 <div>
                     <p>50%  discount for ordering $99</p>
                 </div>
                 <div>
-                    <p>Help & Contact</p>
+                    <li>
+                        <Link href={"#"}>Best deals</Link>
+                        <Link href={"#"}>Help & Contact</Link>
+                    </li>
+
                 </div>
             </div>
 
