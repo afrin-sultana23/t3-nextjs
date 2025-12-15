868c2ae11de7da8f61649c22da21808aa4d79525
afrin-sultana23 <afriin474@gmail.com>
2025-12-15 22:39:41 +0600

    all-products

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -319,6 +319,9 @@ const Page = () => {
 
                     <div className="flex items-center gap-2 mb-3">
                         <div className="flex items-center">
+                            {[...Array(5)].map((_, i) => (
+                                <Star
+                                    key={i}
     );
 };
 
